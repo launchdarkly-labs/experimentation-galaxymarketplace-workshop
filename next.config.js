54d37553c31a0b2cf14c600335dfa00fe9dfe99c
Ahmed Qadri <ahmedhqadri@Ahmeds-MacBook-Pro-2.local>
2025-04-17 17:27:56 -0700

    fixed landing page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/marketplace',
+        permanent: true,
+      },
+    ]
+  },
   env: {
     NEXT_PUBLIC_APP_VERSION: version,
     NEXT_PUBLIC_CREATED_DATE: new Date().toISOString(),
